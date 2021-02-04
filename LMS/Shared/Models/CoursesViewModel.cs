872573a632cd6cdc3268d457b0856f077107138c
srosy <serosenvall@gmail.com>
2021-02-03 17:46:22 -0700

    bug fixed

--- LMS/Shared/Models/CoursesViewModel.cs
+++ LMS/Shared/Models/CoursesViewModel.cs
@@ -7,7 +7,7 @@ namespace LMS.Shared.Models
     {
         public List<Course> Courses { get; set; }
         public List<Account> Professors { get; set; }
-        public readonly Dictionary<string, object> TableAttributes = new Dictionary<string, object>() { { "title", "Select a course to edit." } };
+        public virtual Dictionary<string, object> TableAttributes { get; set; } = new Dictionary<string, object>() { { "title", "Select a course to edit." } };
         public string FormTitle { get; set; }
     }
 }
