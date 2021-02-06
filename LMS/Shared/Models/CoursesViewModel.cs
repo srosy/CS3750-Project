a83aa6f40b0a8303d48fe367ce03c06c0a3f7108
srosy <serosenvall@gmail.com>
2021-02-05 20:03:58 -0700

    components done for the time being

--- LMS/Shared/Models/CoursesViewModel.cs
+++ LMS/Shared/Models/CoursesViewModel.cs
@@ -5,6 +5,7 @@ namespace LMS.Shared.Models
 {
     public class CoursesViewModel
     {
+        public Account Account { get; set; }
         public List<Course> Courses { get; set; }
         public List<Account> Professors { get; set; }
         public virtual Dictionary<string, object> TableAttributes { get; set; } = new Dictionary<string, object>() { { "title", "Select a course to edit." } };
--- LMS/Shared/Models/EnrollmentsViewModel.cs
+++ LMS/Shared/Models/EnrollmentsViewModel.cs
@@ -6,7 +6,8 @@ namespace LMS.Shared.Models
     public class EnrollmentsViewModel : CoursesViewModel
     {
         public Account Account { get; set; }
+        public List<Account> Students { get; set; }
         public List<Enrollment> Enrollments { get; set; }
-        public override Dictionary<string, object> TableAttributes { get; set; } = new Dictionary<string, object>() { { "title", "Select a course to edit." } };
+        public override Dictionary<string, object> TableAttributes { get; set; } = new Dictionary<string, object>() { { "title", "Select to add/remove." } };
     }
 }
