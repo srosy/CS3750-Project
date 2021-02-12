5876265a73fae11ebd6da5e14bc538ddfacc840a
abyssalisk <abyssalisk@gmail.com>
2021-02-11 20:22:27 -0700

    Completed email verification for new accounts.

---
+++ LMS/Shared/Models/CodeVerificationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.Models
+{
+    public class CodeVerificationViewModel
+    {
+        [MaxLength(6)] public string VerificationCode { get; set; }
+    }
+}
