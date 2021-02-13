b38f757d039367fb0c93ba998a2f60b56f43c787
srosy <serosenvall@gmail.com>
2021-02-12 19:21:42 -0700

    Assignments done for professors and students, ready for styling.

--- LMS/Data/Enum.cs
+++ LMS/Data/Enum.cs
@@ -22,4 +22,24 @@
         ANNOUNCEMENT = 2,
         EMAIL = 3
     }
+
+    /// <summary>
+    /// Standard folders registered with the system. These folders are installed with Windows Vista
+    /// and later operating systems, and a computer will have only folders appropriate to it
+    /// installed.
+    /// </summary>
+    public enum KnownFolder
+    {
+        Contacts,
+        Desktop,
+        Documents,
+        Downloads,
+        Favorites,
+        Links,
+        Music,
+        Pictures,
+        SavedGames,
+        SavedSearches,
+        Videos
+    }
 }
