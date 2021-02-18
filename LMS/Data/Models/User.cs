483393d0081e47f92cffe2a15c0e9d97da2f0f02
srosy <serosenvall@gmail.com>
2021-02-17 17:02:24 -0700

    adding a bit more styling and session handling

---
+++ LMS/Data/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LMS.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public int AccountId { get; set; }
+        public int UserType { get; set; }
+    }
+}
---
+++ LMS/Pages/_Host.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages
+{
+    public class HostModel : PageModel
+    {
+        private readonly IHttpContextAccessor _httpContextAccssor;
+
+        public HostModel(IHttpContextAccessor httpContextAccssor)
+        {
+            _httpContextAccssor = httpContextAccssor;
+        }
+
+        public string UserAgent { get; set; }
+        public string IPAddress { get; set; }
+
+        public void OnGet()
+        {
+            UserAgent = _httpContextAccssor.HttpContext.Request.Headers["User-Agent"];
+            IPAddress = _httpContextAccssor.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
