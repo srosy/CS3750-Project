7ac5232f8e8b74dedb1a8709a19d86ae8f7260a9
srosy <serosenvall@gmail.com>
2021-03-03 20:36:53 -0700

    course details assignment charts finished

--- LMS/Shared/Models/BoxPlotChart.cs
+++ LMS/Shared/Models/BoxPlotChart.cs
@@ -1,6 +1,6 @@
 ﻿namespace LMS.Shared.Models
 {
-    public class BellCurveChart
+    public class BoxPlotChart
     {
         public string Name { get; set; }
         public Series[] Series { get; set; }
@@ -9,8 +9,17 @@
     public class Series
     {
         public string Name { get; set; }
-        public object[] Data { get; set; }
+        public SeriesData Data { get; set; }
         public int PointsPossible { get; set; }
         public int AssignmentId { get; set; }
+    }
+
+    public class SeriesData
+    {
+        public int low { get; set; }
+        public int q1 { get; set; }
+        public int median { get; set; }
+        public int q3 { get; set; }
+        public int high { get; set; }
     }
 }
